3135c3e4e021a8b1667276a4df5609672b14065e
Phil-Nowak <philnowak.net>
2017-06-02 11:20:59 +0200

    fin de la v1 d'upload de fichiers

---
+++ EnvoiFichier3.php
@@ -0,0 +1,21 @@
+<!doctype html>
+
+<html>
+	<head>
+		<title>Envoi de fichiers</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="initial-scale=1.0">
+		<link rel="stylesheet" href="../Shared/bootstrap.css" type="text/css">
+	</head>
+
+	<body>
+       <h1>Formulaire d' envoi de fichier(s)</h1>
+        <form method="POST" action="uploadIMG_Files.php" enctype="multipart/form-data">
+            <!--indispensable de mettre le enctype-->
+            <input type="file" name="fichierAEnvoyer">
+            JPG, PNG, GIF 
+            <!--on ne met pas de 'value' dans la balise-->
+            <button type ="submit">Envoyer</button>
+        </form>
+	</body>
+</html>
